2254b60f43d2bce9e5ea0ecc95e7ad441ee83bcc
ismedina <ismaelmedinasuarez@gmail.com>
2020-05-24 12:24:11 +0200

    Minor fix

--- src/contraction_order.jl
+++ src/contraction_order.jl
@@ -1,5 +1,10 @@
 using PyCall
 
+"""
+    simple_dag(cgc)
+
+Returns the lists nodes and edges of a CircuitGateChain Dag
+"""
 function simple_dag(cgc::CircuitGateChain{N}) where N
     nodes = "q" .* string.(1:N)
     qubit_node = [1:N...]
@@ -16,6 +21,7 @@ function simple_dag(cgc::CircuitGateChain{N}) where N
     return nodes, edges
 end
 
+""" Adjacency matrix of a graph"""
 function adj_matrix(nodes, edges)
     N_nodes = length(nodes)
     adj = zeros(Int, N_nodes, N_nodes)
@@ -25,6 +31,12 @@ function adj_matrix(nodes, edges)
     return adj + adj'
 end
 
+"""
+    tree_decomposition(adj)
+
+Constructs a near-optimal tree decomposition of the graph whose adjacency matrix is adj.
+Calls library `Networkx`.
+"""
 function tree_decomposition(adj::AbstractMatrix)
     nx = pyimport("networkx")
     graph_algorithms_approx = pyimport("networkx.algorithms.approximation")
@@ -36,22 +48,27 @@ end
 
 tree_decomposition(cgc::CircuitGateChain) = tree_decomposition(adj_matrix(simple_dag(cgc)...))
 
+"""
+    contraction_order(cgc)
+
+Constructs a near optimal contracting order calling `tree_decomposition`, following
+Theorem 4.6 of https://arxiv.org/abs/quant-ph/0511069.
+"""
 function contraction_order(cgc::CircuitGateChain{N}) where N
     tw, tree = tree_decomposition(cgc)
     ordering = []
     while length(tree.nodes) > 1
-        bags = [bag for bag in tree.nodes]
-        ind_leaf = findfirst([tree.degree(bag) for bag in bags] .== 1)
+        bags = collect(tree.nodes)
+        ind_leaf = findfirst(tree.degree.(bags) .== 1)
         leaf = bags[ind_leaf]
-        for neigh in tree.neighbors(leaf) # there is only one
-            set_leaf = Set(leaf)
-            set_parent = Set(neigh)
-            edge_diff = setdiff(set_leaf, set_parent)
-            for edge in edge_diff
-                push!(ordering,edge)
-            end
-            tree.remove_node(leaf)
+        neighs = collect(tree.neighbors(leaf)); neigh = neighs[1];
+        set_leaf = Set(leaf)
+        set_parent = Set(neigh)
+        edge_diff = setdiff(set_leaf, set_parent)
+        for edge in edge_diff
+            push!(ordering,edge)
         end
+        tree.remove_node(leaf)
     end
     ordering
 end
