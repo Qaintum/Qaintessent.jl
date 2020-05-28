0cd6ea6bc0e894c8c8b841bcbee5225d4415b131
Keefe Huang <huangq1234553@gmail.com>
2020-05-28 15:59:31 +0200

    Added random tests for adder models

--- test/test_models.jl
+++ test/test_models.jl
@@ -3,11 +3,9 @@ using TestSetExtensions
 using LinearAlgebra
 using Qaintessent
 
-
 function isunitary(cb::CircuitGateChain)
     Qaintessent.matrix(cb) * Qaintessent.matrix(Base.adjoint(cb)) ≈ I
 end
-
 
 @testset ExtendedTestSet "quantum Fourier transform" begin
     for N in 1:4
@@ -19,3 +17,99 @@ end
         @test isunitary(c)
     end
 end
+
+@testset ExtendedTestSet "vbe adder test" begin
+    for N in 1:4
+        M = N*3 + 1
+        adder = vbe_adder_circuit(N)
+        ψ = fill(0.0+0.0*im, 2^M)
+        a = abs(rand(Int, 1)[])%(2^N)
+        b = abs(rand(Int, 1)[])%(2^N)
+
+        index = b << N + a
+        ψ[index+1] = 1.0
+
+        ψ = apply(adder, ψ)
+        answer = ((findall(x->x==1, ψ)[1]-1)%(2^2N)) >> N
+        @test answer == (a+b)%(2^N)
+    end
+end
+
+@testset ExtendedTestSet "qcla out of place adder test" begin
+    for N in 1:5
+        n = 1
+        anc= 0
+        while 2^n < N
+            anc += N ÷ 2^n -1
+            n += 1
+        end
+        M = 3N + anc + 1
+
+        cgc = qcla_out_adder_circuit(N)
+
+        a = abs(rand(Int, 1)[])%(2^N)
+        b = abs(rand(Int, 1)[])%(2^N)
+
+        index = b << N + a
+        ψ = fill(0.0+0.0*im, 2^M)
+
+        ψ[index+1] = 1.0
+
+        ψ = apply(cgc, ψ)
+        answer = (findall(x->x==1, ψ)[1] - 1) >> 2N
+        @test answer == a+b
+    end
+end
+
+@testset ExtendedTestSet "qcla in place adder test" begin
+    for N in 1:5
+        n = 1
+        anc= 0
+        while 2^n < N
+            anc += N ÷ 2^n -1
+            n += 1
+        end
+        M = 3N + anc
+
+        cgc = qcla_inplace_adder_circuit(N)
+        a = abs(rand(Int, 1)[])%(2^N)
+        b = abs(rand(Int, 1)[])%(2^N)
+
+        index = b << N + a
+        ψ = fill(0.0+0.0*im, 2^M)
+
+        ψ[index+1] = 1.0
+
+        ψ = apply(cgc, ψ)
+        answer = (findall(x->x==1, ψ)[1] - 1) >> N
+        @test answer == a+b
+    end
+end
+
+# @testset ExtendedTestSet "qcla comparator test" begin
+#     N = 6
+#     cgc = qcla_comparator_circuit(N)
+#     # println(cgc)
+#     for _ in 1:10
+#         n = 1
+#         anc = N - floor(Int, log(2, N-1)) - 2
+#         M = 3N + anc
+#
+#         # cgc = qcla_comparator_circuit(N)
+#         # println(cgc)
+#         a = abs(rand(Int, 1)[])%(2^N)
+#         b = abs(rand(Int, 1)[])%(2^N)
+#
+#         index = b << N + a
+#         ψ = fill(0.0+0.0*im, 2^M)
+#
+#         ψ[index+1] = 1.0
+#
+#         ψ = apply(cgc, ψ)
+#         answer = ((findall(x->x==1, ψ)[1] - 1) >> 2N) % 2
+#         answer = answer == 1 ? true : false
+#         println("A: " * string(a))
+#         println("B: " * string(b))
+#         @test isequal(answer, a>=b)
+#     end
+# end
