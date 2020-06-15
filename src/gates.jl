862ebe1a80e91c4153eec2b0669cad67a54822c6
Keefe Huang <huangq1234553@gmail.com>
2020-06-15 14:50:59 +0200

    Update gates latex markdown

--- src/gates.jl
+++ src/gates.jl
@@ -18,12 +18,12 @@ abstract type AbstractGate{N} end
 """
 Pauli X Matrix
 ``A_{m,n} =
-\begin{pmatrix}
-a_{1,1} & a_{1,2} & \cdots & a_{1,n} \\
-a_{2,1} & a_{2,2} & \cdots & a_{2,n} \\
-\vdots  & \vdots  & \ddots & \vdots  \\
-a_{m,1} & a_{m,2} & \cdots & a_{m,n}
-\end{pmatrix}
+\\begin{pmatrix}
+a_{1,1} & a_{1,2} & \\cdots & a_{1,n} \\\\
+a_{2,1} & a_{2,2} & \\cdots & a_{2,n} \\\\
+\\vdots  & \\vdots  & \\ddots & \vdots \\\\
+a_{m,1} & a_{m,2} & \\cdots & a_{m,n}
+\\end{pmatrix}
 ``
 """
 struct XGate <: AbstractGate{1} end
