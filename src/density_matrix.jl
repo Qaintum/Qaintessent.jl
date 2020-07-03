73cc55e1109eab4872291570e09a313b0ddb404a
Christian B. Mendl <christian.b.mendl@gmail.com>
2020-07-03 08:24:20 +0200

    changed normalization convention for density matrix: representation with respect to 1/2 Pauli matrices

--- src/density_matrix.jl
+++ src/density_matrix.jl
@@ -2,7 +2,7 @@
 """
     DensityMatrix{N}
 
-Density matrix, represented with respect to identity and Pauli basis by storing
+Density matrix, represented with respect to identity and Pauli basis (σ_j/2) by storing
 4 real coefficients for each qubit, i.e., a real vector of length `4^N` for `N` qubits.
 """
 struct DensityMatrix{N}
@@ -18,10 +18,10 @@ end
 
 function matrix(ρ::DensityMatrix{N}) where {N}
     # Pauli matrix basis (including identity matrix)
-    pauli = [Matrix{Float64}(I, 2, 2), matrix(X), matrix(Y), matrix(Z)]
+    halfpauli = [Matrix{Float64}(0.5I, 2, 2), 0.5*matrix(X), 0.5*matrix(Y), 0.5*matrix(Z)]
     mat = zeros(Complex{eltype(ρ.v)}, 2^N, 2^N)
     for (i, pt) in enumerate((cartesian_tuples(4, N)))
-        mat += ρ.v[i] * kron([pauli[p+1] for p in pt]...)
+        mat += ρ.v[i] * kron([halfpauli[p+1] for p in pt]...)
     end
     return mat
 end
@@ -30,10 +30,10 @@ end
 function density_from_statevector(ψ::AbstractVector)
     N = convert(Int, log2(length(ψ)))
     @assert 2^N == length(ψ)
-    halfpauli = [sparse(0.5I, 2, 2), sparse(0.5*matrix(X)), sparse(0.5*matrix(Y)), sparse(0.5*matrix(Z))]
+    pauli = [sparse(I, 2, 2), sparse(matrix(X)), sparse(matrix(Y)), sparse(matrix(Z))]
     v = zeros(4^N)
     for (i, pt) in enumerate((cartesian_tuples(4, N)))
-        v[i] = real(dot(ψ, kron([halfpauli[p+1] for p in pt]...) * ψ))
+        v[i] = real(dot(ψ, kron([pauli[p+1] for p in pt]...) * ψ))
     end
     return DensityMatrix{N}(v)
 end
@@ -47,7 +47,7 @@ encoded as `DensityMatrix{N}`. `ipauli[j]` specifies the j-th matrix as integer 
 """
 function pauli_group_matrix(ipauli::AbstractVector{<:Integer})
     i4 = Matrix{Int}(I, 4, 4)
-    DensityMatrix{length(ipauli)}(kron([i4[i+1, :] for i in reverse(ipauli)]...))
+    DensityMatrix{length(ipauli)}(kron([2*i4[i+1, :] for i in reverse(ipauli)]...))
 end
 
 function pauli_group_matrix(paulistring::String)
