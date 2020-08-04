0b96db06ea4d5840396153d9dceb233f6341a7f5
Keefe Huang <huangq@in.tum.de>
2020-08-04 11:32:08 +0200

    Added tests for qasm support

--- test/test_qasm.jl
+++ test/test_qasm.jl
@@ -10,20 +10,22 @@ using Qaintessent
     qreg a[2];
     qreg c[3];
     creg syn[4];
-    gate syndrome d1,d2,d3,a1,a2
+    gate syndrome(alpha) d1,d2,d3,a1,a2
     {
       cx d1,a1; cx d2,a1;
-      cx d2,a2; cx d3,a2;
+      cx d2,a2; cx d3,a2; ry(alpha) a1;
     }
     gate syn2drome(theta) d1,d2,d3,a1,a2
     {
-        syndrome d1,d2,d3,a1,a2;
-        syndrome d2,d3,a1,d1,a2;
+        syndrome(theta) d1,d2,d3,a1,a2;
+        syndrome(theta) d2,d3,a1,d1,a2;
         rx(theta) d2;
     }
-    syn2drome(2*pi) d[0],d[1],d[2],a[0],a[1];
+    syn2drome(0.1*pi) d[0],d[1],d[2],a[0],a[1];
     if(syn==0) cx d[2],d[1];
-    y d[1];
+    t d[1];
+    s a[0];
+    h d[0];
     """
 
     syn1 = creg(4)
@@ -39,21 +41,23 @@ using Qaintessent
         controlled_circuit_gate(2, 4, X, N),
         controlled_circuit_gate(2, 5, X, N),
         controlled_circuit_gate(3, 5, X, N),
+        single_qubit_circuit_gate(4, RyGate(0.1π), N),
         controlled_circuit_gate(2, 1, X, N),
         controlled_circuit_gate(3, 1, X, N),
         controlled_circuit_gate(3, 5, X, N),
         controlled_circuit_gate(4, 5, X, N),
-        single_qubit_circuit_gate(2, RxGate(2π), N),
+        single_qubit_circuit_gate(1, RyGate(0.1π), N),
+        single_qubit_circuit_gate(2, RxGate(0.1π), N),
         controlled_circuit_gate(3, 2, X, N),
-        single_qubit_circuit_gate(2, Y, N),
+        single_qubit_circuit_gate(2, TGate(), N),
+        single_qubit_circuit_gate(4, SGate(), N),
+        single_qubit_circuit_gate(1, HadamardGate(), N),
     ]
     cgc_ref(gates_ref)
 
     cgc = rbnfparse(src1)
 
     ψ = randn(ComplexF64, 2^N)
-    println(exp(2))
-    ans_ref = apply(cgc_ref, ψ)
 
-    @test ans_ref ≈ ans_ref
+    @test apply(cgc_ref, ψ) ≈ apply(cgc, ψ)
 end
