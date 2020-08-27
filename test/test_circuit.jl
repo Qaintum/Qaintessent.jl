1176625b94ff0e9d1eda8d86858426804e5d8138
Keefe Huang <huangq@in.tum.de>
2020-08-27 15:25:03 +0200

    Updated tests to nest testsets to provide more informatiom after failure

--- test/test_circuit.jl
+++ test/test_circuit.jl
@@ -3,7 +3,6 @@ using TestSetExtensions
 using LinearAlgebra
 using Qaintessent
 
-
 function isunitary(cg::CircuitGate)
     Qaintessent.matrix(cg) * Qaintessent.matrix(Base.adjoint(cg)) ≈ I
 end
@@ -18,77 +17,158 @@ end
     n = randn(3); n /= norm(n)
 
     # single qubit gates
-    for g in [X, Y, Z, HadamardGate(), SGate(), TGate(), RxGate(θ), RyGate(θ), RzGate(θ), RotationGate(θ, n), PhaseShiftGate(ϕ)]
-        cg = CircuitGate((2,), g, 3)
-        @test LinearAlgebra.ishermitian(cg) == (Qaintessent.matrix(cg) == Qaintessent.matrix(adjoint(cg)))
+    @testset "single qubit circuit gates" begin
+        for g in [X, Y, Z, HadamardGate(), SGate(), TGate(), RxGate(θ), RyGate(θ), RzGate(θ), RotationGate(θ, n), PhaseShiftGate(ϕ)]
+            cg = CircuitGate((2,), g, 3)
+            cgadj = adjoint(cg)
+            Qaintessent.matrix(cgadj.gate) == adjoint(Qaintessent.matrix(cg.gate))
+            @test LinearAlgebra.ishermitian(cg) == (Qaintessent.matrix(cg) == Qaintessent.matrix(adjoint(cg)))
+        end
     end
 
     # two qubit gates
-    for g in [controlled_not()]
-        cg = CircuitGate((2, 3), g, 3)
-        @test LinearAlgebra.ishermitian(cg) == (Qaintessent.matrix(cg) == Qaintessent.matrix(adjoint(cg)))
+    @testset "two qubit circuit gates" begin
+        for g in [controlled_not(), SwapGate()]
+            cg = CircuitGate((2, 3), g, 3)
+            cgadj = adjoint(cg)
+            Qaintessent.matrix(cgadj.gate) == adjoint(Qaintessent.matrix(cg.gate))
+            @test LinearAlgebra.ishermitian(cg) == (Qaintessent.matrix(cg) == Qaintessent.matrix(adjoint(cg)))
+        end
     end
 
     # Y acting on second wire
-    cg = CircuitGate((2,), Y, 3)
-    @test Qaintessent.matrix(cg) ≈ kron(kron(Matrix(I, 2, 2), Qaintessent.matrix(Y)), Matrix(I, 2, 2))
-    @test isunitary(cg)
+    @testset "apply circuit gate to second wire" begin
+        cg = CircuitGate((2,), Y, 3)
+        @test Qaintessent.matrix(cg) ≈ kron(kron(Matrix(I, 2, 2), Qaintessent.matrix(Y)), Matrix(I, 2, 2))
+        @test isunitary(cg)
+    end
 
     # flip control and target
-    cg = CircuitGate((2, 1), controlled_not(), 2)
-    @test Qaintessent.matrix(cg) ≈ [1 0 0 0; 0 1 0 0; 0 0 0 1; 0 0 1 0]
-    @test isunitary(cg)
+    @testset "flip control and target circuit gate" begin
+        cg = CircuitGate((2, 1), controlled_not(), 2)
+        @test Qaintessent.matrix(cg) ≈ [1 0 0 0; 0 1 0 0; 0 0 0 1; 0 0 1 0]
+        @test isunitary(cg)
+    end
 
     # third qubit as control and first qubit as target
-    cg = controlled_circuit_gate(3, 1, HadamardGate(), 3)
-    @test Qaintessent.matrix(cg) ≈ [
-        Matrix(I, 4, 4) fill(0, 4, 2) fill(0, 4, 2);
-        fill(0, 2, 4) Qaintessent.matrix(HadamardGate()) fill(0, 2, 2);
-        fill(0, 2, 6) Qaintessent.matrix(HadamardGate())]
-    @test isunitary(cg)
+    @testset "shift control and target circuit gate" begin
+        cg = controlled_circuit_gate(3, 1, HadamardGate(), 3)
+        @test Qaintessent.matrix(cg) ≈ [
+            Matrix(I, 4, 4) fill(0, 4, 2) fill(0, 4, 2);
+            fill(0, 2, 4) Qaintessent.matrix(HadamardGate()) fill(0, 2, 2);
+            fill(0, 2, 6) Qaintessent.matrix(HadamardGate())]
+        @test isunitary(cg)
 
-    cg_test = controlled_circuit_gate(3, 1, HadamardGate(), 3)
-    @test cg_test ≈ cg
+        cg_test = controlled_circuit_gate(3, 1, HadamardGate(), 3)
+        @test cg_test ≈ cg
 
-    cg = single_qubit_circuit_gate(1, RxGate(0.2), 1)
-    cg_test = single_qubit_circuit_gate(1, RxGate(0.2), 1)
+        cg = single_qubit_circuit_gate(1, RxGate(0.2), 1)
+        cg_test = single_qubit_circuit_gate(1, RxGate(0.2), 1)
 
-    @test cg_test ≈ cg
+        @test cg_test ≈ cg
 
-    cg.gate.θ[1] = 0.5
-    @test !(cg ≈ cg_test)
+        cg.gate.θ[1] = 0.5
+        @test !(cg ≈ cg_test)
+    end
+
+    @testset "circuit gate exceptions" begin
+        H = HadamardGate()
+        S = SwapGate()
+
+        N = 1
+        @test_throws ErrorException("Number of gate wires cannot be larger than total number of wires.") CircuitGate{2,N,SwapGate}((1, 2), S)
+
+        N = 2
+        @test_throws ErrorException("Need at least one wire to act on.") CircuitGate{0,N,SwapGate}((), S)
+        @test_throws ErrorException("Wire indices must be unique.") CircuitGate{2,N,SwapGate}((1, 1), S)
+        @test_throws ErrorException("Wire index cannot be smaller than 1.") CircuitGate{2,N,SwapGate}((1, -1), S)
+        @test_throws ErrorException("Wire index cannot be larger than total number of wires.") CircuitGate{2,N,SwapGate}((1, 4), S)
+        @test_throws ErrorException("Gate type must be a subtype of AbstractGate{1}.") CircuitGate{1,N,SwapGate}((1,), S)
+    end
 end
 
-@testset ExtendedTestSet "circuit gate exceptions" begin
-    H = HadamardGate()
-    S = SwapGate()
+@testset ExtendedTestSet "circuit gate isapprox" begin
+    θ = 0.7*π
+    ϕ = 0.4*π
+    n = randn(3); n /= norm(n)
+    ϵ = 3sqrt(eps())
+    sqg = [RxGate(θ), RyGate(θ), RzGate(θ), RotationGate(θ, n), PhaseShiftGate(ϕ)]
+    sqḡ = [RxGate(θ+eps()), RyGate(θ+eps()), RzGate(θ+eps()), RotationGate(θ+eps(), n), PhaseShiftGate(ϕ+eps())]
+    sqĝ = [RxGate(θ+ϵ), RyGate(θ+ϵ), RzGate(θ+ϵ), RotationGate(θ+ϵ, n), PhaseShiftGate(ϕ+ϵ)]
 
-    N = 1
-    @test_throws ErrorException("Number of gate wires cannot be larger than total number of wires.") CircuitGate{2,N,SwapGate}((1, 2), S)
+    for (i,g) in enumerate(sqg)
+        cg1 = CircuitGate((2,), sqg[i], 3)
+        cg2 = CircuitGate((2,), sqḡ[i], 3)
+        cg3 = CircuitGate((2,), sqĝ[i], 3)
+        @test cg1 ≈ cg2
+        @test !(cg1 ≈ cg3)
+    end
+end
 
-    N = 2
-    @test_throws ErrorException("Need at least one wire to act on.") CircuitGate{0,N,SwapGate}((), S)
-    @test_throws ErrorException("Wire indices must be unique.") CircuitGate{2,N,SwapGate}((1, 1), S)
-    @test_throws ErrorException("Wire index cannot be smaller than 1.") CircuitGate{2,N,SwapGate}((1, -1), S)
-    @test_throws ErrorException("Wire index cannot be larger than total number of wires.") CircuitGate{2,N,SwapGate}((1, 4), S)
-    @test_throws ErrorException("Gate type must be a subtype of AbstractGate{1}.") CircuitGate{1,N,SwapGate}((1,), S)
+@testset ExtendedTestSet "circuit gate helper functions" begin
+    N = 5
+    @testset ExtendedTestSet "circuit gate single qubit helper function" begin
+        iwire = rand(1:N)
+        g = XGate()
+        @test single_qubit_circuit_gate(iwire, g, N) ≈ CircuitGate((iwire,), g, N)
+    end
 
+    @testset "circuit gate two qubit helper function" begin
+        iwire1 = rand(1:N)
+        iwire2 = rand(vcat(1:iwire1-1..., iwire1+1:N...))
+        g2 = SwapGate()
+        @test two_qubit_circuit_gate(iwire1, iwire2, g2, N) ≈ CircuitGate((iwire1, iwire2), g2, N)
+    end
+
+    @testset "circuit gate controlled gate helper function" begin
+        cntrl_iwire = rand(1:N)
+        targ_iwire = rand(vcat(1:cntrl_iwire-1..., cntrl_iwire+1:N...))
+        g = YGate()
+        @test controlled_circuit_gate(cntrl_iwire, targ_iwire, g, N) ≈ CircuitGate((cntrl_iwire, targ_iwire), ControlledGate{1,2}(g), N)
+        @test controlled_circuit_gate((cntrl_iwire), targ_iwire, g, N) ≈ CircuitGate((cntrl_iwire, targ_iwire), ControlledGate{1,2}(g), N)
+        @test controlled_circuit_gate(cntrl_iwire, (targ_iwire), g, N) ≈ CircuitGate((cntrl_iwire, targ_iwire), ControlledGate{1,2}(g), N)
+    end
 end
 
 @testset ExtendedTestSet "moments" begin
     N = 3
-    @test_throws ErrorException cgc_ref = CircuitGateChain{N}([
-        Moment{N}(
-        [single_qubit_circuit_gate(2, X, N),
-        single_qubit_circuit_gate(2, HadamardGate(), N),
-        single_qubit_circuit_gate(3, HadamardGate(), N)]
-        ),
-        Moment{N}(
-        [single_qubit_circuit_gate(1, Z, N),
-        single_qubit_circuit_gate(2, Y, N),
-        single_qubit_circuit_gate(1, Y, N)]
-        ),
-    ])
+    @testset "moments constructor" begin
+        g = CircuitGate((2,), XGate(), N)
+        m = Moment{N}(g)
+        @test m[1] === g
+
+        h = CircuitGate((3,), YGate(), N)
+        m = Moment{N}([g,h])
+        @test m[1] === g
+        @test m[2] === h
+    end
+
+    @testset "moments constructor exceptions" begin
+        g = CircuitGate((2,), XGate(), N)
+        h = CircuitGate((2,), YGate(), N)
+        @test_throws ErrorException("Only gates on different wires are allowed in a Moment") cgc_ref =
+            Moment{N}([g, h])
+    end
+
+    @testset "moments adjoint" begin
+        g = CircuitGate((2,), ZGate(), N)
+        h = CircuitGate((1,), SGate(), N)
+        m = Moment{N}([g, h])
+        madj = adjoint(m)
+        @test madj[1] ≈ adjoint(h)
+        @test madj[2] ≈ adjoint(g)
+
+        @test Qaintessent.matrix(adjoint(m)) ≈ adjoint(Qaintessent.matrix(m))
+    end
+
+    @testset "moments reverse" begin
+        g = CircuitGate((2,), ZGate(), N)
+        h = CircuitGate((1,), SGate(), N)
+        m = Moment{N}([g, h])
+        m = reverse(m)
+        @test m[2] ≈ g
+        @test m[1] ≈ h
+    end
 end
 
 @testset ExtendedTestSet "circuit gate chain" begin
@@ -154,8 +234,17 @@ end
     id = Matrix{ComplexF64}(I, 2, 2)
     A = randn(ComplexF64, 2, 2)
     B = randn(ComplexF64, 2, 2)
-    
-    @test sum(kron(A, B) .* rdm(N, (4, 2), ψ, χ)) ≈ sum(kron(A, id, B, id) .* ρ)
+    @testset ExtendedTestSet "reduced density matrix correctness" begin
+        @test sum(kron(A, B) .* rdm(N, (4, 2), ψ, χ)) ≈ sum(kron(A, id, B, id) .* ρ)
+    end
+
+    @testset ExtendedTestSet "reduced density matrix exceptions" begin
+        @test_throws ErrorException("Need at least one wire to act on.") rdm(N, (), ψ, χ)
+        @test_throws ErrorException("Number of gate wires cannot be larger than total number of wires.") rdm(N, (1,2,3,4,5), ψ, χ)
+        @test_throws ErrorException("Wire indices must be unique.") rdm(N, (2,2), ψ, χ)
+        @test_throws ErrorException("Wire index cannot be smaller than 1.") rdm(N, (-1,2), ψ, χ)
+        @test_throws ErrorException("Wire index cannot be larger than total number of wires.") rdm(N, (5,1), ψ, χ)
+    end
 end
 
 @testset ExtendedTestSet "CRegister" begin
@@ -369,5 +458,4 @@ end
         controlled_circuit_gate(3, 6, Y, N),
     ])
     @test all(cgc4 .≈ cgc4_ref)
-
 end
--- test/test_gates.jl
+++ test/test_gates.jl
@@ -13,33 +13,83 @@ isunitary(g::AbstractGate) = Qaintessent.matrix(g) * Qaintessent.matrix(Base.adj
     ϕ = 0.4*π
     n = randn(3); n /= norm(n)
 
-    for g in [X, Y, Z, HadamardGate(), SGate(), TGate(), RxGate(θ), RyGate(θ), RzGate(θ), RotationGate(θ, n), PhaseShiftGate(ϕ), controlled_not()]
-        @test isunitary(g)
-        gdag = adjoint(g)
-        @test LinearAlgebra.ishermitian(g) == (Qaintessent.matrix(gdag) == Qaintessent.matrix(g))
+    @testset "basic quantum gates" begin
+        for g in [X, Y, Z, HadamardGate(), SGate(), TGate(), RxGate(θ), RyGate(θ), RzGate(θ), RotationGate(θ, n), PhaseShiftGate(ϕ), controlled_not()]
+            @test isunitary(g)
+            gdag = adjoint(g)
+
+            @test Qaintessent.matrix(gdag) == adjoint(Qaintessent.matrix(g))
+            @test LinearAlgebra.ishermitian(g) == (Qaintessent.matrix(gdag) == Qaintessent.matrix(g))
+        end
     end
 
-    θ = 6π
-    for g in [RxGate(θ), RyGate(θ), RzGate(θ), PhaseShiftGate(θ)]
-        @test isunitary(g)
-        gdag = adjoint(g)
-        @test LinearAlgebra.ishermitian(g) == (Qaintessent.matrix(gdag) == Qaintessent.matrix(g))
+    @testset "parametrized quantum gates" begin
+        θ = rand(1:50)π
+        for g in [RxGate(θ), RyGate(θ), RzGate(θ), PhaseShiftGate(θ)]
+            @test isunitary(g)
+            gdag = adjoint(g)
+            @test Qaintessent.matrix(gdag) == adjoint(Qaintessent.matrix(g))
+            @test LinearAlgebra.ishermitian(g) == (Qaintessent.matrix(gdag) == Qaintessent.matrix(g))
+        end
     end
 
-    @test Qaintessent.matrix(RotationGate(θ, [1, 0, 0])) ≈ Qaintessent.matrix(RxGate(θ))
-    @test Qaintessent.matrix(RotationGate(θ, [0, 1, 0])) ≈ Qaintessent.matrix(RyGate(θ))
-    @test Qaintessent.matrix(RotationGate(θ, [0, 0, 1])) ≈ Qaintessent.matrix(RzGate(θ))
+    @testset "general rotation gate" begin
+        @test Qaintessent.matrix(RotationGate(θ, [1, 0, 0])) ≈ Qaintessent.matrix(RxGate(θ))
+        @test Qaintessent.matrix(RotationGate(θ, [0, 1, 0])) ≈ Qaintessent.matrix(RyGate(θ))
+        @test Qaintessent.matrix(RotationGate(θ, [0, 0, 1])) ≈ Qaintessent.matrix(RzGate(θ))
 
-    # test MatrixGate
-    N = 3
-    d = 2
-    A = randn(ComplexF64, d^N, d^N)
-    U, _ = qr(A)
-    U = Array(U)
-    gateU = MatrixGate(U)
-    @test Qaintessent.matrix(gateU) ≈ U
-    @test isunitary(gateU)
+        @test Qaintessent.matrix(RotationGate([θ, 0, 0])) ≈ Qaintessent.matrix(RxGate(θ))
+        @test Qaintessent.matrix(RotationGate([0, θ, 0])) ≈ Qaintessent.matrix(RyGate(θ))
+        @test Qaintessent.matrix(RotationGate([0, 0, θ])) ≈ Qaintessent.matrix(RzGate(θ))
+    end
+
+    @testset ExtendedTestSet "general rotation gate exceptions" begin
+        @test_throws ErrorException("Rotation axis vector must have length 3.") Qaintessent.matrix(RotationGate(θ, [1, 0, 0, 0]))
+        @test_throws ErrorException("Norm of rotation axis vector must be 1.") Qaintessent.matrix(RotationGate(θ, [1, 2, 0]))
+
+        @test_throws ErrorException("Rotation axis vector must have length 3.") Qaintessent.matrix(RotationGate([0, θ, 0, 1]))
+    end
+
+    @testset "general unitary gate" begin
+        # test MatrixGate
+        N = 3
+        d = 2
+        A = randn(ComplexF64, d^N, d^N)
+        U, _ = qr(A)
+        U = Array(U)
+        gateU = MatrixGate(U)
+        gateUdag = adjoint(gateU)
+        @test Qaintessent.matrix(gateU) ≈ U
+        @test isunitary(gateU)
+        @test LinearAlgebra.ishermitian(gateU) == (Qaintessent.matrix(gateUdag) == Qaintessent.matrix(gateU))
+
+        U = Qaintessent.matrix(X)
+        gateU = MatrixGate(U)
+        gateUdag = adjoint(gateU)
+        @test Qaintessent.matrix(gateU) ≈ U
+        @test isunitary(gateU)
+        @test true == (Qaintessent.matrix(gateUdag) == Qaintessent.matrix(gateU))
+    end
 
     # not unitary
-    @test_throws ErrorException MatrixGate(randn(ComplexF64, d^N, d^N))
+    @testset "general unitary gate exceptions" begin
+        N = 3
+        d = 2
+        @test_throws ErrorException("Quantum operators must be unitary") MatrixGate(randn(ComplexF64, d^N, d^N))
+    end
+
+    @testset "general controlled gate" begin
+        # test MatrixGate
+        N = 4
+        M = 2
+        d = 2
+        A = randn(ComplexF64, d^M, d^M)
+        U, _ = qr(A)
+        U = Array(U)
+        gateU = MatrixGate(U)
+        cgateU = ControlledGate{M,N}(gateU)
+
+        cgateUdag = adjoint(cgateU)
+        @test LinearAlgebra.ishermitian(cgateU) == (Qaintessent.matrix(cgateU) == Qaintessent.matrix(cgateUdag))
+    end
 end
