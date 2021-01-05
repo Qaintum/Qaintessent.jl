7e7727035308690351331218aa9b46ce50b2eb57
Qunsheng Huang <keefe.huang@tum.de>
2021-01-05 16:33:29 +0100

    Removed CRegister for now, updated implementation of QRegister

--- test/test_register.jl
+++ test/test_register.jl
@@ -7,20 +7,17 @@ using Qaintessent
     @testset "quantum register construction" begin
         N = 3
         q1 = qreg(3)
-        cgc = CircuitGateChain([q1])
-        append!(cgc, [
-            single_qubit_circuit_gate(q1[3], X, N),
-            single_qubit_circuit_gate(q1[1], Y, N),
-            single_qubit_circuit_gate(q1[2], Z, N)
+        cgc = Circuit(q1)
+        add!(X, q1(3))
+        add!(Y, q1(1))
+        add!(Z, q1(2))
+
+        cgc_ref = Circuit{N}([
+            circuit_gate(3, X),
+            circuit_gate(1, Y),
+            circuit_gate(2, Z)
         ])
-        cgc_ref = CircuitGateChain{N}([
-            single_qubit_circuit_gate(3, X, N),
-            single_qubit_circuit_gate(1, Y, N),
-            single_qubit_circuit_gate(2, Z, N)
-        ])
-        for i in 1:length(cgc)
-            @test cgc[i] ≈ cgc_ref[i]
-        end
+        @test matrix(cgc) ≈ matrix(cgc_ref)
     end
 
     @testset "quantum register wire numbers with multiple registers" begin
@@ -28,66 +25,36 @@ using Qaintessent
         q1 = qreg(3)
         q2 = qreg(2)
         q3 = qreg(5)
-        cgc = CircuitGateChain([q1,q2,q3])
-        append!(cgc, [
-            single_qubit_circuit_gate(q1[3], X, N),
-            single_qubit_circuit_gate(q1[1], Y, N),
-            single_qubit_circuit_gate(q1[2], Z, N),
-            controlled_circuit_gate(q2[1], q3[3], Y, N),
-            controlled_circuit_gate(q1[2], q2[2], Z, N),
-            controlled_circuit_gate(q3[2], q3[4], TGate(), N),
-            controlled_circuit_gate(q1[2], q2[1], SGate(), N),
+        cgc = Circuit(q1,q2,q3)
+        
+        add!(X, q1(3))
+        add!(Y, q1(1))
+        add!(Z, q1(2))
+        add_control!(Y, q2(1), q3(3))
+        add_control!(Z, q1(2), q2(2))
+        add_control!(TGate(), q3(2), q3(4))
+        add_control!(SGate(), q1(2), q2(1))
 
-        ])
-        cgc_ref = CircuitGateChain{N}([
-            single_qubit_circuit_gate(3, X, N),
-            single_qubit_circuit_gate(1, Y, N),
-            single_qubit_circuit_gate(2, Z, N),
-            controlled_circuit_gate(4, 8, Y, N),
-            controlled_circuit_gate(2, 5, Z, N),
-            controlled_circuit_gate(7, 9, TGate(), N),
-            controlled_circuit_gate(2, 4, SGate(), N),
+        cgc_ref = Circuit{N}([
+            circuit_gate(3, X),
+            circuit_gate(1, Y),
+            circuit_gate(2, Z),
+            circuit_gate(4, Y, 8),
+            circuit_gate(2, Z, 5),
+            circuit_gate(7, TGate(), 9),
+            circuit_gate(2, SGate(), 4),
             ])
-        for i in 1:length(cgc)
-            @test cgc[i] ≈ cgc_ref[i]
-        end
+        
+        @test matrix(cgc) ≈ matrix(cgc_ref)
     end
 
     @testset "quantum register exceptions" begin
         N = 3
         q1 = qreg(3)
 
-        @test_throws ErrorException("Register object has yet to be used in a CircuitGateChain object") q1[1]
-    end
-
-    @testset "classical register construction" begin
-        c1 = creg(3)
-        q1 = qreg(3)
-        cgc = CircuitGateChain([q1], [c1])
-        set_creg!(c1, 5)
-        @test c1.n == [1,0,1]
-        set_creg!(c1, 3)
-        @test c1.n == [0,1,1]
-        set_creg!(c1, 2)
-        @test c1.n == [0,1,0]
-    end
-
-    @testset "classical register checks" begin
-        c1 = creg(3)
-        q1 = qreg(3)
-        cgc = CircuitGateChain([q1], [c1])
-        set_creg!(c1, 5)
-        @test eval(reg_check(c1, 5))
-        @test !eval(reg_check(c1, 2))
-    end
-
-    @testset "classical register exceptions" begin
-        c1 = creg(3)
-        q1 = qreg(2)
-
-        @test_throws ErrorException("Unable to store integer value 10 in BitArray of size 3") set_creg!(c1, 10)
-
-        cgc = CircuitGateChain([q1], [c1])
-        @test_throws ErrorException("Unable to store integer value 14 in BitArray of size 3") set_creg!(c1, 14)
+        @test_throws ErrorException("Register object has yet to be used in a Circuit object") q1[1]
+        @test_throws ErrorException("Register object has yet to be used in a Circuit object") q1(1)
+        @test_throws ErrorException("Register object has yet to be used in a Circuit object") add!(X, q1(1))
+        @test_throws ErrorException("Register object has yet to be used in a Circuit object") add!(X, q1)
     end
 end
