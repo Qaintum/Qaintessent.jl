3873a6976763f77ad536a4ba51a70eccca393999
Christian B. Mendl <christian.b.mendl@gmail.com>
2021-02-08 22:42:10 +0100

    tailored backward functions for "mixed" apply to density matrix

--- src/gradients_density.jl
+++ src/gradients_density.jl
@@ -18,25 +18,58 @@ backward_density(g::TGate, Δ::AbstractMatrix) = g
 backward_density(g::SdagGate, Δ::AbstractMatrix) = g
 backward_density(g::TdagGate, Δ::AbstractMatrix) = g
 
-backward_density(g::MatrixGate, Δ::AbstractMatrix) = g
-
 
 function backward_density(g::RxGate, Δ::AbstractMatrix)
     c = cos(g.θ[])
     s = sin(g.θ[])
-    RxGate(sum([-s -c; c -s] .* Δ[3:4, 3:4]))
+    RxGate(-s*(Δ[3, 3] + Δ[4, 4]) + c*(Δ[4, 3] - Δ[3, 4]))
 end
+
+function backward_density_mixed_add(g::RxGate, Δ::AbstractMatrix)
+    c = cos(0.5*g.θ[])
+    s = sin(0.5*g.θ[])
+    RxGate(0.5*(-s*tr(Δ) + c*(Δ[4, 3] - Δ[3, 4])))
+end
+
+function backward_density_mixed_sub(g::RxGate, Δ::AbstractMatrix)
+    c = cos(0.5*g.θ[])
+    RxGate(0.5*c*(Δ[1, 2] + Δ[2, 1]))
+end
+
 
 function backward_density(g::RyGate, Δ::AbstractMatrix)
     c = cos(g.θ[])
     s = sin(g.θ[])
-    RyGate(sum([-s -c; c -s] .* Δ[[4, 2], [4, 2]]))
+    RyGate(-s*(Δ[2, 2] + Δ[4, 4]) + c*(Δ[2, 4] - Δ[4, 2]))
 end
+
+function backward_density_mixed_add(g::RyGate, Δ::AbstractMatrix)
+    c = cos(0.5*g.θ[])
+    s = sin(0.5*g.θ[])
+    RyGate(0.5*(-s*tr(Δ) + c*(Δ[2, 4] - Δ[4, 2])))
+end
+
+function backward_density_mixed_sub(g::RyGate, Δ::AbstractMatrix)
+    c = cos(0.5*g.θ[])
+    RyGate(0.5*c*(Δ[1, 3] + Δ[3, 1]))
+end
+
 
 function backward_density(g::RzGate, Δ::AbstractMatrix)
     c = cos(g.θ[])
     s = sin(g.θ[])
-    RzGate(sum([-s -c; c -s] .* Δ[2:3, 2:3]))
+    RzGate(-s*(Δ[2, 2] + Δ[3, 3]) + c*(Δ[3, 2] - Δ[2, 3]))
+end
+
+function backward_density_mixed_add(g::RzGate, Δ::AbstractMatrix)
+    c = cos(0.5*g.θ[])
+    s = sin(0.5*g.θ[])
+    RzGate(0.5*(-s*tr(Δ) + c*(Δ[3, 2] - Δ[2, 3])))
+end
+
+function backward_density_mixed_sub(g::RzGate, Δ::AbstractMatrix)
+    c = cos(0.5*g.θ[])
+    RzGate(0.5*c*(Δ[1, 4] + Δ[4, 1]))
 end
 
 
@@ -61,12 +94,52 @@ function backward_density(g::RotationGate, Δ::AbstractMatrix)
     end
 end
 
+function backward_density_mixed_add(g::RotationGate, Δ::AbstractMatrix)
+    dsn = [Δ[4, 3] - Δ[3, 4], Δ[2, 4] - Δ[4, 2], Δ[3, 2] - Δ[2, 3]]
+    θ = norm(g.nθ)
+    if θ == 0
+        return RotationGate(0.5 * dsn)
+    else
+        n = g.nθ/θ
+        c = cos(0.5*θ)
+        s = sin(0.5*θ)
+        dnθ = -0.5*s * tr(Δ) * n + (0.5*c - s/θ) * dot(n, dsn) * n + (s/θ) * dsn
+        return RotationGate(dnθ)
+    end
+end
+
+function backward_density_mixed_sub(g::RotationGate, Δ::AbstractMatrix)
+    dsn = [Δ[1, 2] + Δ[2, 1], Δ[1, 3] + Δ[3, 1], Δ[1, 4] + Δ[4, 1]]
+    θ = norm(g.nθ)
+    if θ == 0
+        return RotationGate(0.5 * dsn)
+    else
+        n = g.nθ/θ
+        c = cos(0.5*θ)
+        s = sin(0.5*θ)
+        dnθ = (0.5*c - s/θ) * dot(n, dsn) * n + (s/θ) * dsn
+        return RotationGate(dnθ)
+    end
+end
+
 
 function backward_density(g::PhaseShiftGate, Δ::AbstractMatrix)
     # agrees with backward pass for Rz(θ) since global prefactor cancels
     c = cos(g.ϕ[])
     s = sin(g.ϕ[])
     PhaseShiftGate(sum([-s -c; c -s] .* Δ[2:3, 2:3]))
+end
+
+function backward_density_mixed_add(g::PhaseShiftGate, Δ::AbstractMatrix)
+    c = cos(g.ϕ[])
+    s = sin(g.ϕ[])
+    PhaseShiftGate(0.5*(s*((Δ[1, 4] + Δ[4, 1]) - tr(Δ)) + c*(Δ[3, 2] - Δ[2, 3])))
+end
+
+function backward_density_mixed_sub(g::PhaseShiftGate, Δ::AbstractMatrix)
+    c = cos(g.ϕ[])
+    s = sin(g.ϕ[])
+    PhaseShiftGate(0.5*(c*((Δ[1, 4] + Δ[4, 1]) - tr(Δ)) + s*(Δ[2, 3] - Δ[3, 2])))
 end
 
 
@@ -76,27 +149,67 @@ backward_density(g::SwapGate, Δ::AbstractMatrix) = g
 function backward_density(g::EntanglementXXGate, Δ::AbstractMatrix)
     c = cos(g.θ[])
     s = sin(g.θ[])
-    EntanglementXXGate(- s * (Δ[3, 3] + Δ[8, 8] + Δ[4, 4] + Δ[7, 7] + Δ[9,  9] + Δ[14, 14] + Δ[10, 10] + Δ[13, 13])
-                       + c * (Δ[8, 3] - Δ[3, 8] + Δ[4, 7] - Δ[7, 4] + Δ[14, 9] - Δ[ 9, 14] + Δ[13, 10] - Δ[10, 13]))
+    EntanglementXXGate(-s*(Δ[3, 3] + Δ[8, 8] + Δ[4, 4] + Δ[7, 7] + Δ[9,  9] + Δ[14, 14] + Δ[10, 10] + Δ[13, 13])
+                       +c*(Δ[8, 3] - Δ[3, 8] + Δ[4, 7] - Δ[7, 4] + Δ[14, 9] - Δ[ 9, 14] + Δ[13, 10] - Δ[10, 13]))
 end
+
+function backward_density_mixed_add(g::EntanglementXXGate, Δ::AbstractMatrix)
+    c = cos(0.5*g.θ[])
+    s = sin(0.5*g.θ[])
+    EntanglementXXGate(0.5*(-s*tr(Δ) + c*(Δ[4, 7] - Δ[7, 4] + Δ[8, 3] - Δ[3, 8] + Δ[14, 9] - Δ[9, 14] + Δ[13, 10] - Δ[10, 13])))
+end
+
+function backward_density_mixed_sub(g::EntanglementXXGate, Δ::AbstractMatrix)
+    c = cos(0.5*g.θ[])
+    s = sin(0.5*g.θ[])
+    EntanglementXXGate(0.5*c*(Δ[6, 1] + Δ[1, 6] + Δ[5, 2] + Δ[2, 5] + Δ[15, 12] + Δ[12, 15] - Δ[16, 11] - Δ[11, 16]))
+end
+
 
 function backward_density(g::EntanglementYYGate, Δ::AbstractMatrix)
     c = cos(g.θ[])
     s = sin(g.θ[])
-    EntanglementYYGate(- s * (Δ[2,  2] + Δ[12, 12] + Δ[10, 10] + Δ[4,  4] + Δ[5,  5] + Δ[15, 15] + Δ[7,  7] + Δ[13, 13])
-                       + c * (Δ[2, 12] - Δ[12,  2] + Δ[10,  4] - Δ[4, 10] + Δ[5, 15] - Δ[15,  5] + Δ[7, 13] - Δ[13,  7]))
+    EntanglementYYGate(-s*(Δ[2,  2] + Δ[12, 12] + Δ[10, 10] + Δ[4,  4] + Δ[5,  5] + Δ[15, 15] + Δ[7,  7] + Δ[13, 13])
+                       +c*(Δ[2, 12] - Δ[12,  2] + Δ[10,  4] - Δ[4, 10] + Δ[5, 15] - Δ[15,  5] + Δ[7, 13] - Δ[13,  7]))
 end
+
+function backward_density_mixed_add(g::EntanglementYYGate, Δ::AbstractMatrix)
+    c = cos(0.5*g.θ[])
+    s = sin(0.5*g.θ[])
+    EntanglementYYGate(0.5*(-s*tr(Δ) + c*(Δ[10, 4] - Δ[4, 10] + Δ[2, 12] - Δ[12, 2] + Δ[5, 15] - Δ[15, 5] + Δ[7, 13] - Δ[13, 7])))
+end
+
+function backward_density_mixed_sub(g::EntanglementYYGate, Δ::AbstractMatrix)
+    c = cos(0.5*g.θ[])
+    s = sin(0.5*g.θ[])
+    EntanglementYYGate(0.5*c*(Δ[11, 1] + Δ[1, 11] + Δ[9, 3] + Δ[3, 9] + Δ[14, 8] + Δ[8, 14] - Δ[16, 6] - Δ[6, 16]))
+end
+
 
 function backward_density(g::EntanglementZZGate, Δ::AbstractMatrix)
     c = cos(g.θ[])
     s = sin(g.θ[])
-    EntanglementZZGate(- s * (Δ[3,  3] + Δ[14, 14] + Δ[15, 15] + Δ[2,  2] + Δ[12, 12] + Δ[5,  5] + Δ[9, 9] + Δ[8, 8])
-                       + c * (Δ[3, 14] - Δ[14,  3] + Δ[15,  2] - Δ[2, 15] + Δ[12,  5] - Δ[5, 12] + Δ[9, 8] - Δ[8, 9]))
+    EntanglementZZGate(-s*(Δ[3,  3] + Δ[14, 14] + Δ[15, 15] + Δ[2,  2] + Δ[12, 12] + Δ[5,  5] + Δ[9, 9] + Δ[8, 8])
+                       +c*(Δ[3, 14] - Δ[14,  3] + Δ[15,  2] - Δ[2, 15] + Δ[12,  5] - Δ[5, 12] + Δ[9, 8] - Δ[8, 9]))
 end
+
+function backward_density_mixed_add(g::EntanglementZZGate, Δ::AbstractMatrix)
+    c = cos(0.5*g.θ[])
+    s = sin(0.5*g.θ[])
+    EntanglementZZGate(0.5*(-s*tr(Δ) + c*(Δ[15, 2] - Δ[2, 15] + Δ[3, 14] - Δ[14, 3] + Δ[12, 5] - Δ[5, 12] + Δ[9, 8] - Δ[8, 9])))
+end
+
+function backward_density_mixed_sub(g::EntanglementZZGate, Δ::AbstractMatrix)
+    c = cos(0.5*g.θ[])
+    s = sin(0.5*g.θ[])
+    EntanglementZZGate(0.5*c*(Δ[16, 1] + Δ[1, 16] + Δ[13, 4] + Δ[4, 13] + Δ[10, 7] + Δ[7, 10] - Δ[6, 11] - Δ[11, 6]))
+end
+
+
+backward_density(g::MatrixGate, Δ::AbstractMatrix) = g
 
 
 function backward_density(cg::CircuitGate{M,G}, ρ::DensityMatrix, Δ::DensityMatrix) where {M,G}
     @assert ρ.N == Δ.N
     CircuitGate{M,G}(cg.iwire, backward_density(cg.gate, rdm(ρ.N, cg.iwire, Δ.v, ρ.v, 4)))
 end
-
--- test/test_gradients_density.jl
+++ test/test_gradients_density.jl
@@ -32,34 +32,74 @@ end
         Δ = DensityMatrix(randn(Float64, 4), 1)
 
         for g in [RxGate, RyGate, RzGate]
+            θval = 2π*rand()
+
             f(θ) = dot(Δ.v, apply(CircuitGate((1,), g(θ[])), ρ).v)
-            θ = 2π*rand()
-            ngrad = ngradient(f, [θ])
-            dg = Qaintessent.backward_density(g(θ), reshape(kron(ρ.v, Δ.v), 4, 4))
+            ngrad = ngradient(f, [θval])
+            dg = Qaintessent.backward_density(g(θval), reshape(kron(ρ.v, Δ.v), 4, 4))
+            @test isapprox(dg.θ, ngrad[1], rtol=1e-6)
+
+            fa(θ) = dot(Δ.v, Qaintessent.apply_mixed_add(CircuitGate((1,), g(θ[])), ρ).v)
+            ngrad = ngradient(fa, [θval])
+            dg = Qaintessent.backward_density_mixed_add(g(θval), reshape(kron(ρ.v, Δ.v), 4, 4))
+            @test isapprox(dg.θ, ngrad[1], rtol=1e-6)
+
+            fs(θ) = dot(Δ.v, Qaintessent.apply_mixed_sub(CircuitGate((1,), g(θ[])), ρ).v)
+            ngrad = ngradient(fs, [θval])
+            dg = Qaintessent.backward_density_mixed_sub(g(θval), reshape(kron(ρ.v, Δ.v), 4, 4))
             @test isapprox(dg.θ, ngrad[1], rtol=1e-6)
         end
 
         begin
-            f(ϕ) = dot(Δ.v, apply(CircuitGate((1,), PhaseShiftGate(ϕ[])), ρ).v)
-            ϕ = 2π*rand()
-            ngrad = ngradient(f, [ϕ])
-            dg = Qaintessent.backward_density(PhaseShiftGate(ϕ), reshape(kron(ρ.v, Δ.v), 4, 4))
-            @test isapprox(dg.ϕ, ngrad[1], rtol=1e-6)
-        end
+            nθval = randn(3)
 
-        begin
             f(nθ) = dot(Δ.v, apply(CircuitGate((1,), RotationGate(nθ)), ρ).v)
+            ngrad = ngradient(f, nθval)
+            dg = Qaintessent.backward_density(RotationGate(nθval), reshape(kron(ρ.v, Δ.v), 4, 4))
+            @test isapprox(dg.nθ, ngrad[1], rtol=1e-6)
 
-            nθ = randn(3)
-            ngrad = ngradient(f, nθ)
-            dg = Qaintessent.backward_density(RotationGate(nθ), reshape(kron(ρ.v, Δ.v), 4, 4))
+            fa(nθ) = dot(Δ.v, Qaintessent.apply_mixed_add(CircuitGate((1,), RotationGate(nθ)), ρ).v)
+            ngrad = ngradient(fa, nθval)
+            dg = Qaintessent.backward_density_mixed_add(RotationGate(nθval), reshape(kron(ρ.v, Δ.v), 4, 4))
+            @test isapprox(dg.nθ, ngrad[1], rtol=1e-6)
+
+            fs(nθ) = dot(Δ.v, Qaintessent.apply_mixed_sub(CircuitGate((1,), RotationGate(nθ)), ρ).v)
+            ngrad = ngradient(fs, nθval)
+            dg = Qaintessent.backward_density_mixed_sub(RotationGate(nθval), reshape(kron(ρ.v, Δ.v), 4, 4))
             @test isapprox(dg.nθ, ngrad[1], rtol=1e-6)
 
             # special case: zero vector
-            nθ = zeros(3)
-            ngrad = ngradient(f, nθ)
-            dg = Qaintessent.backward_density(RotationGate(nθ), reshape(kron(ρ.v, Δ.v), 4, 4))
+
+            ngrad = ngradient(f, zeros(3))
+            dg = Qaintessent.backward_density(RotationGate(zeros(3)), reshape(kron(ρ.v, Δ.v), 4, 4))
             @test isapprox(dg.nθ, ngrad[1], rtol=1e-6)
+
+            ngrad = ngradient(fa, zeros(3))
+            dg = Qaintessent.backward_density_mixed_add(RotationGate(zeros(3)), reshape(kron(ρ.v, Δ.v), 4, 4))
+            @test isapprox(dg.nθ, ngrad[1], rtol=1e-6)
+
+            ngrad = ngradient(fs, zeros(3))
+            dg = Qaintessent.backward_density_mixed_sub(RotationGate(zeros(3)), reshape(kron(ρ.v, Δ.v), 4, 4))
+            @test isapprox(dg.nθ, ngrad[1], rtol=1e-6)
+        end
+
+        begin
+            ϕval = 2π*rand()
+
+            f(ϕ) = dot(Δ.v, apply(CircuitGate((1,), PhaseShiftGate(ϕ[])), ρ).v)
+            ngrad = ngradient(f, [ϕval])
+            dg = Qaintessent.backward_density(PhaseShiftGate(ϕval), reshape(kron(ρ.v, Δ.v), 4, 4))
+            @test isapprox(dg.ϕ, ngrad[1], rtol=1e-6)
+
+            fa(ϕ) = dot(Δ.v, Qaintessent.apply_mixed_add(CircuitGate((1,), PhaseShiftGate(ϕ[])), ρ).v)
+            ngrad = ngradient(fa, [ϕval])
+            dg = Qaintessent.backward_density_mixed_add(PhaseShiftGate(ϕval), reshape(kron(ρ.v, Δ.v), 4, 4))
+            @test isapprox(dg.ϕ, ngrad[1], rtol=1e-6)
+
+            fs(ϕ) = dot(Δ.v, Qaintessent.apply_mixed_sub(CircuitGate((1,), PhaseShiftGate(ϕ[])), ρ).v)
+            ngrad = ngradient(fs, [ϕval])
+            dg = Qaintessent.backward_density_mixed_sub(PhaseShiftGate(ϕval), reshape(kron(ρ.v, Δ.v), 4, 4))
+            @test isapprox(dg.ϕ, ngrad[1], rtol=1e-6)
         end
     end
 
@@ -72,11 +112,23 @@ end
         Δ = DensityMatrix(randn(Float64, 16), 2)
 
         for g in [EntanglementXXGate, EntanglementYYGate, EntanglementZZGate]
+            θval = 2π*rand()
+
             f(θ) = dot(Δ.v, apply(CircuitGate((1, 2), g(θ[])), ρ).v)
-            θ = 2π*rand()
-            ngrad = ngradient(f, [θ])
-            dg = Qaintessent.backward_density(g(θ), reshape(kron(ρ.v, Δ.v), 16, 16))
+            ngrad = ngradient(f, [θval])
+            dg = Qaintessent.backward_density(g(θval), reshape(kron(ρ.v, Δ.v), 16, 16))
+            @test isapprox(dg.θ, ngrad[1], rtol=1e-6)
+
+            fa(θ) = dot(Δ.v, Qaintessent.apply_mixed_add(CircuitGate((1, 2), g(θ[])), ρ).v)
+            ngrad = ngradient(fa, [θval])
+            dg = Qaintessent.backward_density_mixed_add(g(θval), reshape(kron(ρ.v, Δ.v), 16, 16))
+            @test isapprox(dg.θ, ngrad[1], rtol=1e-6)
+
+            fs(θ) = dot(Δ.v, Qaintessent.apply_mixed_sub(CircuitGate((1, 2), g(θ[])), ρ).v)
+            ngrad = ngradient(fs, [θval])
+            dg = Qaintessent.backward_density_mixed_sub(g(θval), reshape(kron(ρ.v, Δ.v), 16, 16))
             @test isapprox(dg.θ, ngrad[1], rtol=1e-6)
         end
     end
+
 end
