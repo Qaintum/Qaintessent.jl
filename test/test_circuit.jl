18ab2fd5b8af5b14f6b72079a090298d483fc3ab
Qunsheng Huang <keefe.huang@tum.de>
2021-03-04 18:22:04 +0100

    Added test for circuit construction and measurement operator adding

--- test/test_circuit.jl
+++ test/test_circuit.jl
@@ -6,42 +6,113 @@ using Qaintessent
 
 ##==----------------------------------------------------------------------------------------------------------------------
 @testset ExtendedTestSet "circuit construction" begin
-    N = 1
-    cgs = [
-        circuit_gate(1, X),
-        circuit_gate(1, HadamardGate()),
-        circuit_gate(1, Z),
-        circuit_gate(1, Y),
-        ]
-    ref_moments = Moment.(cgs)
-    meas = MeasurementOperator(Matrix{Float64}(I, 2^N, 2^N), (1,))
-    c = Circuit{N}(cgs, [meas])
-    for i in 1:length(cgs)
-        @test c[i] ≈ ref_moments[i]
-    end
-    ψ = randn(ComplexF64, 2^N)
-    @test distribution(c, ψ) ≈ apply(c.moments, ψ)
-
-    ψs = apply(cgs, ψ)
-    ψref = apply(c, ψ)
-    
-    @test [dot(ψs, apply(meas, ψs))] ≈ apply(c, ψ)
-end
-
-@testset ExtendedTestSet "circuit construction with circuit gate measurements" begin
     N = 3
-    cgc = [
+    cgs = [
         circuit_gate(1, X),
         circuit_gate(2, HadamardGate()),
         circuit_gate(3, Z),
         circuit_gate(1, Y),
+        ]
+    ref_moments = Moment[
+        Moment([circuit_gate(1, X),
+                circuit_gate(2, HadamardGate()),
+                circuit_gate(3, Z)]),
+        Moment([circuit_gate(1, Y)])
     ]
-    iwires = [(1,), (2,), (3,)]
-    meas = MeasurementOperator.([X, X, X], iwires)
-    c = Circuit{N}(cgc, meas)
-    ψ = randn(ComplexF64, 2^N)
-    @test distribution(c, ψ) ≈ apply(c.moments, ψ)
+    
+    @testset "basic circuit construction" begin
+        c = Circuit{N}(cgs)
+        
+        for i in 1:length(c)
+            @test c[i] ≈ ref_moments[i]
+        end
 
-    ψs = apply(cgc, ψ)
-    @test [dot(ψs, apply(m, ψs)) for m in meas] ≈ apply(c, ψ)
+        ψ = randn(ComplexF64, 2^N)
+        @test distribution(c, ψ) ≈ apply(c.moments, ψ)
+
+        @test_throws ErrorException("Circuit does not contain any measurement operators") apply(c, ψ)
+    end
+
+    @testset "circuit construction without gates" begin
+        meas = mop(Matrix{Float64}(I, 2^N, 2^N), (1,2,3))
+        
+        c = Circuit{N}()
+
+        ψ = randn(ComplexF64, 2^N)
+
+        @test_throws ErrorException("Circuit does not contain any gates") apply(c, ψ)
+    end
+
+    @testset "circuit construction with measurements" begin
+        iwires = [(1,), (2,), (3,)]
+        meas = mop.([X, X, X], iwires)
+        c = Circuit{N}(cgs, meas)
+        ψ = randn(ComplexF64, 2^N)
+        @test distribution(c, ψ) ≈ apply(c.moments, ψ)
+
+        ψs = apply(cgs, ψ)
+        @test [dot(ψs, apply(m, ψs)) for m in meas] ≈ apply(c, ψ)
+    end
+end
+
+@testset ExtendedTestSet "circuit helper functions" begin
+    N = 3
+    cgs = [
+        circuit_gate(1, X),
+        circuit_gate(2, HadamardGate()),
+        circuit_gate(3, Z),
+        circuit_gate(1, Y),
+        ]
+    ref_moments = Moment[
+        Moment([circuit_gate(1, X),
+                circuit_gate(2, HadamardGate()),
+                circuit_gate(3, Z)]),
+        Moment([circuit_gate(1, Y)])
+    ]
+    @testset "circuit add measurement operator" begin
+        c = Circuit{N}(cgs)
+
+        meas = mop(X, 1)
+        add_measurement!(c, meas)
+
+        ψ = randn(ComplexF64, 2^N)
+        ψs = apply(cgs, ψ)
+
+        @test [dot(ψs, apply(meas, ψs))] ≈ apply(c, ψ)
+    end
+
+    @testset "circuit add measurement operators" begin
+        c = Circuit{N}(cgs)
+
+        meas = mop.([X, X, X], (1,2,3))
+        add_measurement!(c, meas)
+
+        ψ = randn(ComplexF64, 2^N)
+        ψs = apply(cgs, ψ)
+
+        @test [dot(ψs, apply(m, ψs)) for m in meas] ≈ apply(c, ψ)
+    end
+
+    @testset "circuit add measurement operator exceptions" begin
+        meas = mop.([X, X, X], (1,2,3))
+        c = Circuit{N}(cgs, meas)
+
+        meas2 = mop(Y, 1)
+        @test_throws ErrorException("Measurement operators do not commute") add_measurement!(c, meas2)
+        @test_throws ErrorException("Measurement operators do not commute") add_measurement!(c, [meas2])
+
+        meas3 = mop(X, 5)
+        @test_throws ErrorException("Measurement operators affecting 5 wires provided for circuit of size 3") add_measurement!(c, meas3)
+        @test_throws ErrorException("Measurement operators affecting 5 wires provided for circuit of size 3") add_measurement!(c, [meas3])
+    end
+
+    @testset "circuit reverse" begin
+        c = Circuit{N}(cgs, meas)
+        reverse_c = reverse(c)
+        reverse_ref_moments = reverse(ref_moments)
+        for i in 1:length(c_new)
+            @test reverse_c[i] ≈ reverse_ref_moments[i]
+            @test c[i] ≈ ref_moments[i]
+        end
+    end
 end
