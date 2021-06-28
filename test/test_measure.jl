3178e1e36d4df75d18a61282db74105f59d8d649
Martin Knudsen <31985420+martin-knudsen@users.noreply.github.com>
2021-06-28 12:18:35 +0200

    Update test_measure.jl

--- test/test_measure.jl
+++ test/test_measure.jl
@@ -17,6 +17,8 @@ using StatsBase
         state_a = ComplexF64[1;0;0;0]
         circ = [circuit_gate(1, HadamardGate())]
         state_b = apply(state_a, circ)
-        ave = measure(state_b, 20000)
+        samples = measure(state_b, 20000)
+        # Measurement result should follow binomial distribution, this should return false ~1 out of a trillion
+        @test samples[0]<10500 && samples[0]>9500
     end
 end
