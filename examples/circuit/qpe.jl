a9d8e6ab3c4fb5b1865185c2840e27c2b7a61643
Qunsheng Huang <ga53vuw@tum.de>
2021-10-08 13:21:22 +0200

    Adding qpe example to examples

--- examples/circuit/qpe.jl
+++ examples/circuit/qpe.jl
@@ -1,16 +1,21 @@
-using Test
-using TestSetExtensions
-using RBNF
 using Qaintessent
+using Random
+using LinearAlgebra
 using JLD2
+using BenchmarkTools
 
 
 # Adding random comments and spacings in src1 to test robustness of lexer
-f = open("qpe.txt")
-s = read(f, String)
+# f = open("qpe.txt")
+# s = read(f, String)
 
 # println(Qaintessent.parse_qasm(Qaintessent.lex(s)))
 
-cgc = qasm2cgc(s)
-JLD2.save_object("cgc.jld2", cgc)
-# println(cgc)
+# cgc = qasm2cgc(s)
+# JLD2.save_object("cgc.jld2", cgc)
+cgc = JLD2.load_object("cgc.jld2")
+
+ψ = rand(ComplexF64, 2^16)
+ψ = ψ ./ norm(ψ)
+ψ = Statevector(ψ)
+apply!(ψ, cgc.moments)
