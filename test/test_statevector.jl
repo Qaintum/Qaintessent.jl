821e6d4241edd465956eb15b9d70348a7bd7418d
Qunsheng Huang <ga53vuw@tum.de>
2021-11-12 19:57:28 +0100

    Remove dobled test

--- test/test_statevector.jl
+++ test/test_statevector.jl
@@ -63,4 +63,28 @@ using Qaintessent
             @test_throws ErrorException("Wire index cannot be larger than total number of wires.") rdm(N, (5, 1), ψ, χ)
         end
     end
+
+    @testset ExtendedTestSet "util reduced density matrix for statevector" begin
+        N = 4
+        ψ = Statevector(randn(ComplexF64, 2^N))
+        χ = Statevector(randn(ComplexF64, 2^N))
+        # full density matrix |ψ⟩⟨χ|
+        ρ = reshape(kron(conj(χ.state), ψ.state), 2^N, 2^N)
+        id = Matrix{ComplexF64}(I, 2, 2)
+        A = randn(ComplexF64, 2, 2)
+        B = randn(ComplexF64, 2, 2)
+        @testset ExtendedTestSet "reduced density matrix correctness" begin
+            @test sum(kron(A, B) .* rdm(N, (4, 2), ψ, χ)) ≈ sum(kron(B, id, A, id) .* ρ)
+        end
+    
+        @testset ExtendedTestSet "reduced density matrix exceptions" begin
+            @test_throws ErrorException("Need at least one wire to act on.") rdm(N, (), ψ, χ)
+            @test_throws ErrorException("Number of gate wires cannot be larger than total number of wires.") rdm(N, (1, 2, 3, 4, 5), ψ, χ)
+            @test_throws ErrorException("Wire indices must be unique.") rdm(N, (2, 2), ψ, χ)
+            @test_throws ErrorException("Wire index cannot be smaller than 1.") rdm(N, (-1, 2), ψ, χ)
+            @test_throws ErrorException("Wire index cannot be larger than total number of wires.") rdm(N, (5, 1), ψ, χ)
+        end
+    end
+
+
 end
